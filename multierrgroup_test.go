bcdbf6194a0a6a2a6da200b1de47986df08b6cb3
Brandon LeBlanc <brandon@leblanc.codes>
2023-03-14 00:54:35 +0000

    implement basic tests

---
+++ multierrgroup_test.go
@@ -0,0 +1,43 @@
+package multierrgroup_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/demosdemon/cpanic"
+	"github.com/demosdemon/multierrgroup"
+	"github.com/hashicorp/go-multierror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWait(t *testing.T) {
+	var g multierrgroup.Group
+
+	g.Go(func() error {
+		time.Sleep(10 * time.Millisecond)
+		return nil
+	})
+
+	g.Go(func() error {
+		time.Sleep(10 * time.Millisecond)
+		return errors.New("error")
+	})
+
+	g.Go(func() error {
+		time.Sleep(10 * time.Millisecond)
+		panic("oh noes")
+	})
+
+	err := g.Wait()
+	assert.NotNil(t, err)
+
+	merr, ok := err.(*multierror.Error)
+	assert.True(t, ok)
+	assert.Len(t, merr.Errors, 2)
+	assert.Equal(t, "error", merr.Errors[0].Error())
+
+	p, ok := merr.Errors[1].(*cpanic.Panic)
+	assert.True(t, ok)
+	assert.Equal(t, "panic: oh noes", p.Error())
+}
